e36be83c068ea7100fcd209607f40e5340c79b9d
howkmii <abdelhakim@univ-tiaret.dz>
2021-09-20 20:01:31 +0100

    enable infinite scroll

--- lib/main.dart
+++ lib/main.dart
@@ -82,7 +82,7 @@ class _MyHomePageState extends State<MyHomePage> {
           Text("mSlider 1 initial page Index 0\n\n",textAlign: TextAlign.center,),
           CarouselSlider(
             //1st methode
-            options: CarouselOptions(height: 186,initialPage:0 ,enlargeCenterPage: true, autoPlay: true, autoPlayAnimationDuration: Duration(seconds:1 )),
+            options: CarouselOptions(height: 186,initialPage:0 ,enlargeCenterPage: true, autoPlay: true, autoPlayAnimationDuration: Duration(seconds:1 ), enableInfiniteScroll: false),
             items: imgList.map((imageUrl) {
               return Container(
                 width: double.infinity,
