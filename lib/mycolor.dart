23b73ada26a682a46bc4a416e04b20f44951cf28
howkmii <abdelhakim@univ-tiaret.dz>
2021-09-27 20:38:42 +0100

    InheritedWidget

--- lib/mycolor.dart
+++ lib/mycolor.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class MyColor extends InheritedWidget {
   @override
   bool updateShouldNotify(covariant InheritedWidget oldWidget) {
-    // TODO: implement updateShouldNotify
+    
     throw UnimplementedError();
   }
 
