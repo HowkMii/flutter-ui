c062c499d3992d613b16cb89381a1e91117cbf6d
mostefa <21nounou21@gmail.com>
2021-09-30 20:36:03 +0100

    adding widget

--- android/app/src/main/kotlin/com/example/flutterui/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutterui/MainActivity.kt
@@ -3,4 +3,9 @@ package com.example.flutterui
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    var x = findViewById("hello")
+    x.setText("hello")
+    for(i in 10){
+        x.setTextColor("#122134")
+    }
 }
